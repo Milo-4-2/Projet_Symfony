3e00080d28c6d0e5b77409d5c32c307b69ac389e
Milo-4-2 <milo.vidal402@gmail.com>
2025-03-04 15:26:19 +0100

    database artist entity

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -11,8 +11,6 @@ final class HomePageController extends AbstractController
     #[Route('/', name: 'app_home_page')]
     public function index(): Response
     {
-        return $this->render('home_page/index.html.twig', [
-            'controller_name' => 'HomePageController',
-        ]);
+        return $this->render('home_page/index.html.twig');
     }
 }
